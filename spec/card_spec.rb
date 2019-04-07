41ab193399437ae76b46afafdef1631285e52453
mythlabs <amitbisht744@gmail.com>
2019-04-07 16:05:01 +0530

    Adding custom error message to example

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,19 +10,28 @@ class Card
 end
 
 RSpec.describe Card do #creates an example group
-    before do
-        def card
-            Card.new('Ace', 'Spades')
-        end
-        
-    end
+    # before do
+    #     def card
+    #         Card.new('Ace', 'Spades')
+    #     end
+    # end
+
+    let(:card) { Card.new('Ace','Spades')}
 
     #what it should do #termed as an example
-    it 'has a ranki' do
+    it 'has a rank and that rank can change' do
         expect(card.rank).to eq('Ace')
+        card.rank = 'a'
+        expect(card.rank).to eq('a')
     end
 
     it 'has a suit' do
         expect(card.suit).to eq('Spades')
     end
+
+    it 'has custom error message' do
+        comparison = 'Spades'
+        expect(card.suit).to eq(comparison), "I expected #{comparison} but got #{card.suit}, you dumbass"
+    end
+
 end
